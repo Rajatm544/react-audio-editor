af9e56405639de2265fcf9ac473cc2693d90b58c
Rajatm544 <rajatm544@gmail.com>
2021-12-02 20:35:44 +0530

    add the two pages needed

---
+++ src/pages/EditPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditPage = () => {
+	return (
+		<div>
+			<h1>EDIT THE AUDIO HERE</h1>
+		</div>
+	);
+};
+
+export default EditPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UploadAudio from '../components/UploadAudio';
+
+const HomePage = () => {
+	return (
+		<div>
+			<UploadAudio />
+		</div>
+	);
+};
+
+export default HomePage;
