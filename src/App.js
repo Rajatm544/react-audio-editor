af81bbe76b362e6bd2465c966a82b75d923b8b9f
Rajatm544 <rajatm544@gmail.com>
2021-12-02 20:35:21 +0530

    add router to add the required pages to the react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import EditPage from './pages/EditPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<HomePage />} exact />
+				<Route path='/edit' element={<EditPage />} exact />
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
