bda94e742d73eaf17e61edfb927dd5c41d128f63
Rajatm544 <rajatm544@gmail.com>
2021-12-04 23:20:21 +0530

    add footer and navbar to app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EditPage from './pages/EditPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
 	return (
 		<Router>
+			<Navbar />
 			<Switch>
 				<Route path='/' component={HomePage} exact />
 				<Route path='/edit' component={EditPage} exact />
 			</Switch>
+			<Footer />
 		</Router>
 	);
 };
